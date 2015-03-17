ab770518b21b0a36f7639fe8dbbc6b65457e1ddd
BrendaLBorges <brendalemes97@hotmail.com>
2015-03-17 11:09:14 -0300

    Update exercicio1_2.java
    ex2

--- exercicio1/exercicio1_2.java
+++ exercicio1/exercicio1_2.java
@@ -1,4 +1,4 @@
-public class Exercicio1_2{
+public class exercicio1_2{
         public static void main (String [] args){
                 String argumento = args[0];
                 System.out.println("Digite algo " + argumento);
