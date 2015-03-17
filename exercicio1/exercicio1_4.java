7cd42a0a502782208b80a3f1fa34eb1ad414174e
BrendaLBorges <brendalemes97@hotmail.com>
2015-03-17 11:10:38 -0300

    Create exercicio1_4.java
    ex4

---
+++ exercicio1/exercicio1_4.java
@@ -0,0 +1,19 @@
+public class Exercicio1_4{
+	public static void main (String []args{
+
+		int A;
+		A=45;
+		float B=20;
+		float C=3;
+		float SOMA=A+B+C;
+		float SUBTRACAO=A-B-C;
+		float DIVISAO=A/B/C;
+		float MULTIPLICACAO=A*B*C
+		
+	System.out.println (“A SOMA FOI:”+ SOMA);
+	System.out.println (“A SUBTRACAO FOI:”+ SUBTRACAO);
+	System.out.println (“A DIVISAO FOI:”+ DIVISAO);
+	System.out.println (“A MULTIPLICACAO FOI:”+ MULTIPLICACAO);
+		
+    }
+}
