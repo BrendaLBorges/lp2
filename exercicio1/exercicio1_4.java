cadfacc0e8b91d22c3d5733947883e9ef5da3aec
BrendaLBorges <brendalemes97@hotmail.com>
2015-03-18 13:03:01 -0300

    Update exercicio1_4.java
    ex4

--- exercicio1/exercicio1_4.java
+++ exercicio1/exercicio1_4.java
@@ -1,19 +1,18 @@
-public class Exercicio1_4{
-	public static void main (String []args{
+public class exercicio1_4{
+	public static void main (String [] args{
 
-		int A;
-		A=45;
-		float B=20;
-		float C=3;
-		float SOMA=A+B+C;
-		float SUBTRACAO=A-B-C;
-		float DIVISAO=A/B/C;
-		float MULTIPLICACAO=A*B*C
+		float a=45;
+		float b=20;
+		float c=3;
+		float soma=a+b+c;
+		float subtracao=a-b-c;
+		float divisao=a/b/c;
+		float multiplicacao=a*b*c;
 		
-	System.out.println (“A SOMA FOI:”+ SOMA);
-	System.out.println (“A SUBTRACAO FOI:”+ SUBTRACAO);
-	System.out.println (“A DIVISAO FOI:”+ DIVISAO);
-	System.out.println (“A MULTIPLICACAO FOI:”+ MULTIPLICACAO);
+	System.out.println (“A SOMA FOI:”+ soma);
+	System.out.println (“A SUBTRACAO FOI:”+ subtracao);
+	System.out.println (“A DIVISAO FOI:”+ divisao);
+	System.out.println (“A MULTIPLICACAO FOI:”+ multiplicacao);
 		
     }
 }
