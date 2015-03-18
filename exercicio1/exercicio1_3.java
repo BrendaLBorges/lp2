0c09d5f9e89340fa4997094a39302f0daae33739
BrendaLBorges <brendalemes97@hotmail.com>
2015-03-18 12:47:04 -0300

    Update exercicio1_3.java
    ex3

--- exercicio1/exercicio1_3.java
+++ exercicio1/exercicio1_3.java
@@ -1,21 +1,15 @@
-public class exercicio1_3{
-	public static void main (String []args){
-	
-
-		
-		int A;
-		A=45;
-		int B=20;
-		int C=3;
-		int SOMA=A+B+C;
-		int SUBTRACAO=A-B-C;
-		int DIVISAO=A/B/C;
-		int MULTIPLICACAO=A*B*C;
-		
-	System.out.println (“A SOMA FOI:”+ SOMA);
-	System.out.println (“A SUBTRACAO FOI:”+ SUBTRACAO);
-	System.out.println (“A DIVISAO FOI:”+ DIVISAO);
-	System.out.println (“A MULTIPLICACAO FOI:”+ MULTIPLICACAO);
-		
-    }
+public class Exercicio1_3{
+        public static void main (String [] args){
+                int a = 45;
+                int b = 20;
+                int c = 3;
+                int soma = a+b+c;
+                int subtracao = a-b-c;
+                int multiplicacao = a*b*c;
+                int divisao = a/b/c;
+                System.out.println ("O valor da soma foi: " + soma);
+                System.out.println ("O valor da subtracao foi: " + subtracao);
+                System.out.println ("O valor da multiplicacao foi: " + multiplicacao);
+                System.out.println ("O valor da divisao foi: " + divisao);
+        }
 }
