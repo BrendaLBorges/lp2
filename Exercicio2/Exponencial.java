3b1181bc033933c191457c07aa737765e6029642
BrendaLBorges <brendalemes97@hotmail.com>
2015-03-26 14:49:35 -0300

    Create Exponencial.java
    potencia de potenciaa

---
+++ Exercicio2/Exponencial.java
@@ -0,0 +1,16 @@
+public class Exponencial{
+
+public static void main (String [] agrs){
+
+System.out.println("#\tq\tc");
+
+for (int i = 0; i <= 10; i++){
+
+System.out.printf("%d\t%d\t%d%n", i, i*i, i*i*i);
+
+  }
+
+
+  }
+
+}
